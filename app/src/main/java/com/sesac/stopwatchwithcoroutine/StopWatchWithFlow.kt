5784d98b9702ec440fe5a367ee39bf8355b00f33
yswn1531 <yswn1531@gmail.com>
2024-01-07 11:36:24 +0900

    Refactor: 코드 정렬 및 재사용 코드 정리

--- app/src/main/java/com/sesac/stopwatchwithcoroutine/StopWatchWithFlow.kt
+++ app/src/main/java/com/sesac/stopwatchwithcoroutine/StopWatchWithFlow.kt
@@ -8,6 +8,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.sesac.stopwatchwithcoroutine.common.DELAY_TIME
 import com.sesac.stopwatchwithcoroutine.common.getMilliseconds
 import com.sesac.stopwatchwithcoroutine.common.getMinutes
 import com.sesac.stopwatchwithcoroutine.common.getSeconds
@@ -41,12 +42,21 @@ class StopWatchWithFlow : AppCompatActivity() {
         binding = ActivityFlowBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        with(binding) {
+        clickStartButton()
+        clickResetButton()
+    }
+
+    /**
+     * Start Button Click Event
+     *
+     */
+    private fun clickStartButton(){
+        with(binding){
             startBtn.setOnClickListener {
                 isRunning = !isRunning
                 if (isRunning) {
                     mainTimerJob = defaultCoroutineScope.launch {
-                        timerStart()
+                        mainTimerStart()
                     }
                     if (::subTimerJob.isInitialized && subTimerJob.isCancelled && repeatedTime != 0) {
                         subTimerJob = defaultCoroutineScope.launch {
@@ -57,7 +67,15 @@ class StopWatchWithFlow : AppCompatActivity() {
                     pause()
                 }
             }
+        }
+    }
 
+    /**
+     * Reset Button Click Event
+     *
+     */
+    private fun clickResetButton(){
+        with(binding) {
             resetBtn.setOnClickListener {
                 if (!mainTimerJob.isActive) {
                     reset()
@@ -70,22 +88,15 @@ class StopWatchWithFlow : AppCompatActivity() {
 
             }
         }
-        //emit은 flowOn으로 emit하나?
     }
 
-    private suspend fun timerStart() {
-        CoroutineScope(Dispatchers.Main).launch {
-            with(binding) {
-                startBtn.setBackgroundColor(Color.RED)
-                startBtn.text = resources.getString(R.string.stop)
-                resetBtn.text = resources.getString(R.string.split_timer)
-            }
-        }
+    private suspend fun mainTimerStart() {
+        settingWhenStartButtonUI()
         while (true) {
             val timerFlow = flowOf(repeatedTime)
                 .map {
                     repeatedTime++
-                    delay(delayTime)
+                    delay(DELAY_TIME)
                 }.flowOn(Dispatchers.Default)
             timerFlow.collect {
                 val minute = repeatedTime.getMinutes()
@@ -102,6 +113,24 @@ class StopWatchWithFlow : AppCompatActivity() {
         }
     }
 
+    /**
+     * 시작버튼 눌렀을 때 Button UI
+     *
+     */
+    private fun settingWhenStartButtonUI() {
+        CoroutineScope(Dispatchers.Main).launch {
+            with(binding) {
+                startBtn.setBackgroundColor(Color.RED)
+                startBtn.text = resources.getString(R.string.stop)
+                resetBtn.text = resources.getString(R.string.split_timer)
+            }
+        }
+    }
+
+    /**
+     * Sub timer start 구간기록 타이머
+     *
+     */
     private suspend fun subTimerStart() {
         while (true) {
             val timerFlow = flowOf(repeatedTimeSub)
@@ -115,41 +144,119 @@ class StopWatchWithFlow : AppCompatActivity() {
                 val milliseconds = repeatedTimeSub.getMilliseconds()
                 CoroutineScope(Dispatchers.Main).launch {
                     with(binding) {
-                        tempMinuteText.text = String.format("%02d", minute)
-                        tempSecondText.text = String.format("%02d", seconds)
-                        tempMilliSecondText.text = String.format("%02d", milliseconds)
+                        subMinuteText.text = String.format("%02d",minute)
+                        subSecondText.text = String.format("%02d",seconds)
+                        subMilliSecondText.text = String.format("%02d",milliseconds)
                     }
                 }
             }
         }
     }
 
+    /**
+     * Pause 정지
+     *
+     */
     private fun pause() {
-        mainTimerJob.cancel()
-        if (::subTimerJob.isInitialized && subTimerJob.isActive) subTimerJob.cancel()
-        binding.startBtn.setBackgroundColor(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.basic
-            )
-        )
-        binding.startBtn.text = resources.getString(R.string.resume)
-        binding.resetBtn.text = resources.getString(R.string.reset)
+        coroutineCancel()
+        settingWhenPauseButtonUI()
     }
 
+    /**
+     * 정지버튼 눌렀을 떄 Button UI
+     *
+     */
+    private fun settingWhenPauseButtonUI(){
+        with(binding){
+            startBtn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.basic))
+            startBtn.text = resources.getString(R.string.resume)
+            resetBtn.text = resources.getString(R.string.reset)
+        }
+    }
+
+    /**
+     * Reset 초기화
+     *
+     */
     private fun reset() {
-        mainTimerJob.cancel()
-        if (::subTimerJob.isInitialized && subTimerJob.isActive) subTimerJob.cancel()
+        coroutineCancel()
         initTimer()
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun currentTapTime() {
-        binding.linearLayout.visibility = View.VISIBLE
-        binding.linearLayout2.visibility = View.VISIBLE
-        binding.line.visibility = View.VISIBLE
-        binding.scoll1.visibility = View.VISIBLE
+    /**
+     * Timer 초기화
+     *
+     */
+    private fun initTimer() {
+        isRunning = false
+        repeatedTime = 0
+        repeatedTimeSub = 0
+        saveIndex = 1
+        resetTimerText()
+        resetButtonUI()
+        resetTimeLabUI()
+    }
 
+    /**
+     * 구간 기록 UI 초기화
+     *
+     */
+    private fun resetTimeLabUI() {
+        with(binding){
+            indexLayout.visibility = View.INVISIBLE
+            subTimerLayout.visibility = View.INVISIBLE
+            line.visibility = View.INVISIBLE
+            timeLabScroll.visibility = View.INVISIBLE
+            labLayout.removeAllViews()
+            labLayout.invalidate()
+        }
+    }
+
+    /**
+     * 타이머 TEXT 초기화
+     *
+     */
+    private fun resetTimerText(){
+        with(binding){
+            minuteText.text = resources.getString(R.string.init_zero)
+            secondText.text = resources.getString(R.string.init_zero)
+            milliSecondText.text = resources.getString(R.string.init_zero)
+            subMinuteText.text = resources.getString(R.string.init_zero)
+            subSecondText.text = resources.getString(R.string.init_zero)
+            subMilliSecondText.text = resources.getString(R.string.init_zero)
+        }
+    }
+
+    /**
+     * Button UI 초기화
+     *
+     */
+    private fun resetButtonUI(){
+        with(binding){
+            startBtn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.basic))
+            startBtn.text = resources.getString(R.string.start)
+            resetBtn.text = resources.getString(R.string.split_timer)
+        }
+    }
+
+    /**
+     * 구간 기록 기능
+     *
+     */
+    private fun currentTapTime() {
+        startTimeLabUI()
+        addCurrentTapTime()
+        if (::subTimerJob.isInitialized && subTimerJob.isActive) subTimerJob.cancel()
+        repeatedTimeSub = 0
+        saveIndex++
+    }
+
+    /**
+     * 구간 기록 버튼 눌렀을 때 추가 되는 View
+     *
+     */
+    @SuppressLint("SetTextI18n")
+    private fun addCurrentTapTime() {
         val labTimeTV = TextView(this).apply {
             textSize = 20f
         }
@@ -162,22 +269,10 @@ class StopWatchWithFlow : AppCompatActivity() {
         val subMilliseconds = String.format("%02d", repeatedTimeSub.getMilliseconds())
 
         with(labTimeTV) {
-            if (saveIndex == 1) {
-                text =
-                    """         ${
-                        String.format(
-                            "%02d",
-                            saveIndex
-                        )
-                    }             $minute:$seconds.$milliseconds        $minute:$seconds.$milliseconds     """
+            text = if(saveIndex == 1) {
+                """         ${String.format("%02d",saveIndex)}             $minute:$seconds.$milliseconds        $minute:$seconds.$milliseconds     """
             } else {
-                text =
-                    """         ${
-                        String.format(
-                            "%02d",
-                            saveIndex
-                        )
-                    }             $subMinute:$subSeconds.$subMilliseconds        $minute:$seconds.$milliseconds     """
+                """         ${String.format("%02d",saveIndex)}             $subMinute:$subSeconds.$subMilliseconds        $minute:$seconds.$milliseconds     """
             }
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -185,11 +280,21 @@ class StopWatchWithFlow : AppCompatActivity() {
             ).also {
                 it.setMargins(0, 15, 0, 15)
             }
-            binding.lapLayout.addView(labTimeTV, 0)
+            binding.labLayout.addView(labTimeTV, 0)
         }
-        if (::subTimerJob.isInitialized && subTimerJob.isActive) subTimerJob.cancel()
-        repeatedTimeSub = 0
-        saveIndex++
+    }
+
+    /**
+     * 구간 기록 시작 UI
+     *
+     */
+    private fun startTimeLabUI() {
+        with(binding){
+            subTimerLayout.visibility = View.VISIBLE
+            indexLayout.visibility = View.VISIBLE
+            line.visibility = View.VISIBLE
+            timeLabScroll.visibility = View.VISIBLE
+        }
     }
 
     override fun onStop() {
@@ -197,36 +302,16 @@ class StopWatchWithFlow : AppCompatActivity() {
         coroutineCancel()
     }
 
+    /**
+     * Coroutine cancel
+     *
+     */
     private fun coroutineCancel() {
         if (::mainTimerJob.isInitialized && mainTimerJob.isActive) {
             mainTimerJob.cancel()
         }
         if (::subTimerJob.isInitialized && subTimerJob.isActive) {
             subTimerJob.cancel()
-        }
-    }
-
-    private fun initTimer() {
-        isRunning = false
-        repeatedTime = 0
-        repeatedTimeSub = 0
-        saveIndex = 1
-        with(binding) {
-            startBtn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.basic))
-            minuteText.text = resources.getString(R.string.init_zero)
-            secondText.text = resources.getString(R.string.init_zero)
-            milliSecondText.text = resources.getString(R.string.init_zero)
-            tempMinuteText.text = resources.getString(R.string.init_zero)
-            tempSecondText.text = resources.getString(R.string.init_zero)
-            tempMilliSecondText.text = resources.getString(R.string.init_zero)
-            lapLayout.removeAllViews()
-            lapLayout.invalidate()
-            startBtn.text = resources.getString(R.string.start)
-            resetBtn.text = resources.getString(R.string.split_timer)
-            linearLayout.visibility = View.INVISIBLE
-            linearLayout2.visibility = View.INVISIBLE
-            line.visibility = View.INVISIBLE
-            scoll1.visibility = View.INVISIBLE
         }
     }
 
